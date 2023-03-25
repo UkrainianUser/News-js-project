9cb61806453a03c1414c43fe05f944a1df5d5f74
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-26 01:11:47 +0200

    weather

---
+++ src/index.js
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+import "./js/theme-toggle";
+// import './js/mobile-menu';
+import './js/fetchNews';
+import './js/fetchNewsApi';
+import "./js/weatherBase";
+import './js/renderCard';
+import './js/storage';
+import './js/pagination';
+
+
+import './js/home-page';
+import './js/favorite';
+import './js/read';
+
+>>>>>>> Stashed changes
