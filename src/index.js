a4e93fa5bc7e30dcb30c6f40b685b7d02de38da9
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-03-27 11:48:36 +0300

    index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import './js/theme-toggle';
-// import './js/mobile-menu';
+import './js/mobile-menu';
 import './js/fetchNews';
 import './js/fetchNewsApi';
 import './js/weatherBase';
@@ -8,7 +8,7 @@ import './js/renderCard';
 import './js/storage';
 import './js/pagination';
 import './js/saerchNews';
-// import './js/categories';
+import './js/categories';
 
 import './js/home-page';
 import './js/favorite';
