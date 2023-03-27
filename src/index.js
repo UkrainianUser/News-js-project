a954b11c0be5e0a5ee27fda74ed5b66e8c507d42
Natali-1991 <natawa.bodnar@mail.ru>
2023-03-27 14:41:28 +0100

    bugfixSwitcher

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-
+import './js/read';
 import './js/theme-toggle';
 import './js/mobile-menu';
+import './js/mobile-theme-toggle';
 import './js/fetchNews';
 import './js/fetchNewsApi';
-import './js/weatherBase';
 import './js/renderCard';
 import './js/storage';
 import './js/pagination';
-import './js/saerchNews';
-import './js/categories';
 
 import './js/home-page';
 import './js/favorite';
