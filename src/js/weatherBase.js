0dffff34251c5218f35f29d878c0e650d3054978
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-27 23:51:48 +0300

    fix-weather

--- src/js/weatherBase.js
+++ src/js/weatherBase.js
@@ -1,23 +1,23 @@
 import weather, { getWeather }  from "./weather";
 
-const weatherForm = document.querySelector('.weather');
+const weatherForm = document.querySelector('.card-news__list');
+const numberNews = document.querySelector('.card-news__number2');
+const windowInnerWidth = window.innerWidth
 
  async function addWeather() {
   try {
     const response = await getWeather();
     const data = await response.data;
-    const createMarkup = `
+    const createMarkup = `<li class = "weather">
     <div class="weather__block"><span class="weather__number">${Math.round(data.main.temp)}&#176;</span>
       <div class = "weather__location"><h2 class="weather__title">${data.weather[0].main}</h2>
       <p class="weather__city"><svg class="weather__svg" width="16" height="16">
       <use href="/symbol-defs.33050382.svg#icon-carbon_location-filled"></use>
     </svg>${data.name}</p></div></div>
       <img class="weather__icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">
-      <p class="weather__date">${new Date(data.dt*1000).toDateString()}</p>`
-      
-
-    return weatherForm.insertAdjacentHTML('afterbegin', createMarkup);
-  
+      <p class="weather__date">${new Date(data.dt*1000).toDateString()}</p></li>`
+        
+          return weatherForm.insertAdjacentHTML('afterbegin', createMarkup);  
   } catch (error) {
     console.error(error);
   }
