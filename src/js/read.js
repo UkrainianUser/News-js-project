3cf79cf247da35a03a28783973e6dc45d05759f0
Denys07748 <denis07748@gmail.com>
2023-04-03 22:12:15 +0300

    add plug to peage read

--- src/js/read.js
+++ src/js/read.js
@@ -8,22 +8,26 @@ const READ_KEY = 'readNews';
 const FAVORITE_KEY = 'favoriteNews';
 
 const dateWrapper = document.querySelector('.date__wrapper');
+const plugEl = document.querySelector('.wrapper-plug');
 
 getReadNews();
 
 function getReadNews() {
+  plugEl.classList.add('is-hidden');
   if (load(READ_KEY).length) {
-      const readNews = load(READ_KEY);
-      const normalizeNews = readNews.filter((value) => value);
-      const rangeDate = getSortDate(readNews);
-      console.log(rangeDate);
-      const readObj = rangeDate.reduce((obj, date) => {
-        obj[date] = normalizeNews.filter((value) => {
-          return isSameDay(new Date(date), new Date(value.data));
-        });
-        return obj;
-      }, {});
-      renderCardRead(readObj);
+    const readNews = load(READ_KEY);
+    const normalizeNews = readNews.filter(value => value);
+    const rangeDate = getSortDate(readNews);
+    console.log(rangeDate);
+    const readObj = rangeDate.reduce((obj, date) => {
+      obj[date] = normalizeNews.filter(value => {
+        return isSameDay(new Date(date), new Date(value.data));
+      });
+      return obj;
+    }, {});
+    renderCardRead(readObj);
+  } else {
+    plugEl.classList.remove('is-hidden');
   }
 }
 
@@ -33,7 +37,11 @@ function getSortDate(news) {
   });
   console.log(dateArray);
 
-  const sortDateArray = dateArray.filter((item) => {return item }).sort(compareDesc);
+  const sortDateArray = dateArray
+    .filter(item => {
+      return item;
+    })
+    .sort(compareDesc);
 
   const rangeDate = eachDayOfInterval({
     start: new Date(sortDateArray[sortDateArray.length - 1]),
@@ -48,9 +56,9 @@ function handleClickBtn(event) {
     return;
   }
 
-  event.target.textContent = "Remove from favorite";
-  event.target.style.backgroundColor = "#4B48DB";
-  event.target.style.color = "#fff";
+  event.target.textContent = 'Remove from favorite';
+  event.target.style.backgroundColor = '#4B48DB';
+  event.target.style.color = '#fff';
 
   const favoritNewsId = event.target.dataset.id;
   const parsedReadNews = load(READ_KEY);
@@ -73,8 +81,9 @@ function handleClickBtn(event) {
 
 function renderCardRead(readObj) {
   const markup = Object.keys(readObj)
-  .filter((item) => {
-    return readObj[item].length !== 0;})
+    .filter(item => {
+      return readObj[item].length !== 0;
+    })
     .map((item, index) => {
       return `  <li class="date__item" data-index=${index}>
         <div class="date__card"><h3 class="date__text">${lightFormat(
@@ -89,7 +98,8 @@ function renderCardRead(readObj) {
 }
 
 function getText(news) {
-  const markup = news.map(({id, title, paragraph, img, data, url, category, multimedia}) => {
+  const markup = news
+    .map(({ id, title, paragraph, img, data, url, category, multimedia }) => {
       return `<li class="card-news__item read">
       <div class="card-news__ovarlay read">
         <img
@@ -110,12 +120,15 @@ function getText(news) {
     <p class="card-news__text read">
       ${paragraph}
     </p>
-    <time class="card-news__time read">${lightFormat(new Date(data
-        ), 'dd/MM/yyyy')}</time>
+    <time class="card-news__time read">${lightFormat(
+      new Date(data),
+      'dd/MM/yyyy'
+    )}</time>
     <a data-url=${url} class="card-news__element read" href=${url} target="_blank" rel="noreferrer noopener">Read more</a>
-  </li>`
-}).join("");
-return markup;
+  </li>`;
+    })
+    .join('');
+  return markup;
 }
 
 dateWrapper.addEventListener('click', handleClickItem);
